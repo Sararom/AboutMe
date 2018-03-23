3ac278747ca75b6b7a56fc12d3bf4054085b6bb9
Sara <00030716@uca.edu.sv>
2018-03-23 00:21:24 -0600

    AboutMe

---
+++ app/src/main/java/com/romero/aboutme/MainActivity.java
@@ -0,0 +1,19 @@
+package com.romero.aboutme;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
